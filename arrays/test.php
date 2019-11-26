59d278fbfb2982a79f12ab37f6a0280c2b00d0be
Andre Lauk <andre.lauk@khk.ee>
2019-11-26 09:32:11 +0200

    Form, ruumala arvutused

---
+++ arrays/test.php
@@ -0,0 +1,20 @@
+<?php ?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>06 - PHP - Vormid</title>
+    </head>
+    <body>
+        <form action="test.php" method="get">
+            Kera raadius <input type="text" name="kera_raadius">
+            <hr>
+            Koonuse raadius <input type="text" name="koonuse_raadius"><br>
+            Koonuse kõrgus <input type="text" name="koonuse_korgus"><br>
+            <hr>
+            Silindri raadius <input type="text" name="silindri_raadius"><br>
+            Silindri kõrgus <input type="text" name="silindri_korgus"><br>
+            <input type="submit" value="Saada">
+        </form>
+    </body>
+</html>
